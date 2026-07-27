0f95909eb7f5da61808efd7a8f8c00bc1c6a9a75
Salma Sherif <S.Sherif2371@nu.edu.eg>
2026-07-27 14:22:02 +0300

    Feature(Production): add batch details screen

--- lib/features/authentication/login/login_controller.dart
+++ lib/features/authentication/login/login_controller.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:juhayna_smart_production_and_inventory_management_system/core/services/auth_service.dart';
 import '../../dashboard/dashboard_screen.dart';
 import 'package:juhayna_smart_production_and_inventory_management_system/core/services/user_service.dart';
+import 'package:juhayna_smart_production_and_inventory_management_system/features/production/dashboard/production_dashboard_screen.dart';
 
 class LoginController {
   final BuildContext context;
@@ -24,15 +26,53 @@ class LoginController {
         password: passwordController.text,
       );
 
+      print("Logged in email: ${FirebaseAuth.instance.currentUser?.email}");
+      print("Logged in UID: ${FirebaseAuth.instance.currentUser?.uid}");
+
+      print("===== AFTER SIGN IN =====");
+
       final user = await UserService.getCurrentUser();
+
+      print("User object: $user");
       print("Name: ${user?.name}");
       print("Role: ${user?.role}");
       print("Department: ${user?.department}");
 
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (_) => const DashboardScreen()),
-      );
+      print("===== BEFORE SWITCH =====");
+      if (user == null) {
+        throw Exception("User data not found.");
+      }
+
+      switch (user.role) {
+        case 'Production Supervisor':
+          print(">>>>>>>> Opening Production Dashboard <<<<<<<<");
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (_) => const ProductionDashboardScreen(),
+            ),
+          );
+          break;
+
+        case 'Warehouse Staff':
+          // TODO: Replace with WarehouseDashboardScreen when created
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => const DashboardScreen()),
+          );
+          break;
+
+        case 'Manager':
+          // TODO: Replace with ManagerDashboardScreen when created
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => const DashboardScreen()),
+          );
+          break;
+
+        default:
+          throw Exception("Unknown user role: ${user.role}");
+      }
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text(e.toString()), backgroundColor: Colors.red),
